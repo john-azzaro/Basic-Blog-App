ed7156a962b1e79a17dace5d2c256031217e263b
John Azzaro <jazzaro@temple.edu>
2019-11-01 21:40:25 -0700

    updated server creation

--- server.js
+++ server.js
@@ -6,4 +6,7 @@ const mongoose = require('mongoose');                                           
 const app = express();                                                                   //2.1
 
 
+app.listen(3000, function() {
+    console.log(`Your app is listening on port 3000`);
+});
 
