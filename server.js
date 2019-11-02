b5c1bcc090d09670f30651bedeac4ce718bc4389
John Azzaro <jazzaro@temple.edu>
2019-11-01 21:30:07 -0700

    updates to server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const express = require('express');                                                      //2.1
+const mongoose = require('mongoose');                                                    //2.2
+
+const app = express();                                                                   //2.1
+
+
+
