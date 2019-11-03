7e984c7dc5843d023ef0374de7fd6b91f374eed3
John Azzaro <jazzaro@temple.edu>
2019-11-02 23:01:41 -0700

    closeServer added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const express = require('express');                                                      // 2.1 - Import Express
-const mongoose = require('mongoose');                                                    // 3.1 - Import Mongoose 
+const express = require('express');                                                      // 2.1 - Import Express.
+const mongoose = require('mongoose');                                                    // 3.1 - Import Mongoose.
 
-mongoose.Promise = global.Promise;                                                       // 3.2 - Configure Mongoose to use ES6 Promises
+mongoose.Promise = global.Promise;                                                       // 3.2 - Configure Mongoose to use ES6 Promises.
 
-const { DATABASE_URL, PORT } = require('./config');                                      // 3.5 - Import DATABASE_URL and PORT from config.js file
+const { DATABASE_URL, PORT } = require('./config');                                      // 3.5 - Import DATABASE_URL and PORT from config.js file.
 
 const app = express();                                                                   // 2.1 - Instantiate the app using Express.
 
@@ -14,9 +14,9 @@ const app = express();                                                          
 
 // Server and Database connection...
 
-let server; 
+let server;                                                                               // 3.6 - Declare server outside runServer.
         
-function runServer(databaseUrl, port=PORT) {  
+function runServer(databaseUrl, port=PORT) {                                              // 3.7 - Connect to database and run HTTP server.
     return new Promise((resolve, reject) => {    
         mongoose.connect(databaseUrl, { useNewUrlParser: true,  useUnifiedTopology: true }, err => { 
             if (err) {          
@@ -35,8 +35,7 @@ function runServer(databaseUrl, port=PORT) {
     });
 }  
 
-
-function closeServer() {
+function closeServer() {                                                                    // 3.8 - Close server (for integration tests).
     return mongoose.disconnect().then(() => {
       return new Promise((resolve, reject) => {
         console.log('Closing server');
