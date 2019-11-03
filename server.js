e54c71ef2cab677ae83ea89393e8bf5ada6de1ce
John Azzaro <jazzaro@temple.edu>
2019-11-02 17:35:47 -0700

    added config, mongoose to server

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 'use strict';
 
 const express = require('express');                                                      //2.1
-const mongoose = require('mongoose');                                                    //2.2
+const mongoose = require('mongoose');                                                    //3.1
 
 const app = express();                                                                   //2.1
 
 
-app.listen(3000, function() {
-    console.log(`Your app is listening on port 3000`);
-});
-
+mongoose.Promise = global.Promise;                                                       //3.2
