65c86a008e350d370ef78262def00593829e4db1
John Azzaro <jazzaro@temple.edu>
2019-11-03 15:31:28 -0800

    revised server layout

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 'use strict';
 
+                                                                                         // Imports.
 const express = require('express');                                                      // 2.1 - Import Express.
 const mongoose = require('mongoose');                                                    // 3.1 - Import Mongoose.
-
+const morgan = require('morgan');                                                        // 4.2 - Import Morgan.
 mongoose.Promise = global.Promise;                                                       // 3.2 - Configure Mongoose to use ES6 Promises.
 
+                                                                                         // Imported from files.
 const { DATABASE_URL, PORT } = require('./config');                                      // 3.5 - Import DATABASE_URL and PORT from config.js file.
 
+                                                                                         // Instantiate the app.
 const app = express();                                                                   // 2.1 - Instantiate the app using Express.
 
-
-// Middleware
-
-app.use(express.json());
-
+                                                                                         // Middleware
+app.use(express.json());                                                                 // 4.1 - Install built-in middleware like express.json.
+app.use(morgan('common'));                                                               // 4.2 - Install third-party middleware like morgan.
 
 
 // Server and Database connection...
