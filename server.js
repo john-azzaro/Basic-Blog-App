87c4507a17c7608fa215dd51aaf1d34735f9a84d
John Azzaro <jazzaro@temple.edu>
2019-11-02 19:13:50 -0700

    importing values from config

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const express = require('express');                                                      //2.1
-const mongoose = require('mongoose');                                                    //3.1
+const express = require('express');                                                      // 2.1 - Import Express
+const mongoose = require('mongoose');                                                    // 3.1 - Import Mongoose 
 
-const app = express();                                                                   //2.1
+mongoose.Promise = global.Promise;                                                       // 3.2 - Configure Mongoose to use ES6 Promises
+
+const { DATABASE_URL, PORT } = require('./config');                                      // 3.5 - Import DATABASE_URL and PORT from config.js file
+
+const app = express();                                                                   // 2.1 - Instantiate the app using Express.
 
 
-mongoose.Promise = global.Promise;                                                       //3.2
+
