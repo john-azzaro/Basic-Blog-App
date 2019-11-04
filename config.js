c6ca228cdca7315436fe284f261a18a6db36d8d9
John Azzaro <jazzaro@temple.edu>
2019-11-03 21:36:11 -0800

    fixed database

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app';                   //3.4
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/basic-blog-app';             //3.4
 exports.PORT = process.env.PORT || 3000;                                                             //3.4
