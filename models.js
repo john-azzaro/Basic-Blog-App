8848541a728dff32b73e2dbef95808efe3d3e9c4
John Azzaro <jazzaro@temple.edu>
2019-11-04 22:28:12 -0800

    added blog schema

--- models.js
+++ models.js
@@ -1,6 +1,15 @@
 'use strict';
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');                       //
+mongoose.Promise = global.Promise;                          //
 
-mongoose.Promise = global.Promise;
+const blogPostSchema = mongoose.Schema({                    //
+    author: { 
+        firstName: String,
+        lastName: String
+    },
+    title: {type: String, required: true}, 
+    content: {type: String}, 
+    created: {type: Date, default: Date.now}
+});
 
