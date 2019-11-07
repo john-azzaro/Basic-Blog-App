0b54da2e0753ce2100bb90f5bcde01c62a1db74e
John Azzaro <jazzaro@temple.edu>
2019-11-06 16:03:26 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
 'use strict';
 
-                                                                                         // Imports.
+                                                                                         // Imports:
 const express = require('express');                                                      // 2.1 - Import Express.
 const mongoose = require('mongoose');                                                    // 3.1 - Import Mongoose.
 const morgan = require('morgan');                                                        // 4.2 - Import Morgan.
 mongoose.Promise = global.Promise;                                                       // 3.2 - Configure Mongoose to use ES6 Promises.
 
-                                                                                         // Imported from files.
+                                                                                         // Imported from files:
 const { DATABASE_URL, PORT } = require('./config');                                      // 3.5 - Import DATABASE_URL and PORT from config.js file.
 
-                                                                                         // Instantiate the app.
+                                                                                         // Instantiate the app:
 const app = express();                                                                   // 2.1 - Instantiate the app using Express.
 
-                                                                                         // Middleware
+                                                                                         // Middleware:
 app.use(express.json());                                                                 // 4.1 - Install built-in middleware like express.json.
 app.use(morgan('common'));                                                               // 4.2 - Install third-party middleware like morgan        (note: phase 5 is in models.js).
+
+                                                                                         // Routes:
+app.get('/posts', (req, res) => {                                                        // 
+
+});
+ 
+app.get('/posts/:id', (req, res) => {
+
+});
+
+app.post('/posts', (req, res) => {
+
+});
+
+app.put('/posts/:id', (req, res) => {
+
+});
+
+app.delete('posts/:id', (req, res) => {
+
+});
+
+app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Not Found' });
+});
+
+
+
+
+
+
+
 
                                                                                          // Server and Database connection...
 let server;                                                                              // 3.6 - Declare server outside runServer.
